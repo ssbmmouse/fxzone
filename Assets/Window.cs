59aed598e105d30d133aa9cf71100f95f4a57e5e
ssbmmouse <48599508+ssbmmouse@users.noreply.github.com>
2019-03-15 12:02:02 -0500

    Added early (basically unusable) project

---
+++ Assets/Window.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Window : MonoBehaviour
+{
+    SpriteRenderer r;
+    // Start is called before the first frame update
+    void Start()
+    {
+        r = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetRandomColor() {
+        r.color = new Color(Random.Range(0,0.5f),Random.Range(0,0.5f),Random.Range(0,0.5f),Random.Range(0.05f,0.2f));
+    }
+}
---
+++ README.txt
@@ -0,0 +1,7 @@
+This program takes a collection of .mp4 files and will play them in a random order with random visual effects.
+This may be useful for people creating video content on YouTube who have a collection of clips and want to 
+quickly put them together. The user could then record the output of the program, and use that in a video editor.
+
+HOW TO USE
+Currently, put the files in the specified path, and number each clip from 1 up until the max number of clips.
+So, if there are 10 clips, you should have files named 1,2,3,4... 10. 
